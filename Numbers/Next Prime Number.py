6c30c57c0bfba2fe96b8ba285936994c5e496cd8
PruteanuVlad <69005128+PruteanuVlad@users.noreply.github.com>
2020-12-05 22:37:29 +0200

    Update Next Prime Number.py

--- Numbers/Next Prime Number.py
+++ Numbers/Next Prime Number.py
@@ -1,3 +1,7 @@
+import _thread
+import msvcrt
+
+
 def prime(n):
     if n==2:
         return 1
@@ -7,3 +11,21 @@ def prime(n):
         if n%i==0:
             return 0
     return 1
+
+
+def input_thread(a_list):
+    msvcrt.getche()
+    a_list.append(True)
+
+
+def do_stuff():
+    a_list = []
+    _thread.start_new_thread(input_thread, (a_list,))
+    n=2
+    while not a_list:
+        if prime(n):
+            print(n,end=' ')
+        n=n+1
+
+print("The program will display the prime numbers. Press any key to exit")
+do_stuff()
