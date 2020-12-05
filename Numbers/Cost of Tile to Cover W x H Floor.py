e26aab3bb5d7b93464145c808a72acf63bad3000
PruteanuVlad <69005128+PruteanuVlad@users.noreply.github.com>
2020-12-05 22:47:01 +0200

    Create Cost of Tile to Cover W x H Floor.py

---
+++ Numbers/Cost of Tile to Cover W x H Floor.py
@@ -0,0 +1,4 @@
+w=float(input("Enter the width W of the floor:"))
+h=float(input("Enter the height H of the floor:"))
+p=float(input("Enter the price per square meter P of the floor:"))
+print("The cost to cover the floor is:",w*h*p)
